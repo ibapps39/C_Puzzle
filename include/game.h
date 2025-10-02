16349ee197b9c5562b83bf2115b70f4dab714d4e
Ian Brown <ibapps39@gmail.com>
2025-10-02 07:23:04 -0600

    consolidated files

--- include/game.h
+++ include/game.h
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+
 #include "raylib.h"
 #include "rlgl.h"
 
@@ -36,7 +37,9 @@ typedef struct
     const char* playerName;
     Camera3D* playerCamera;
     Vector3* currentPOS;
-    Vector3* velocity;         // Store velocity vector
+    Vector3* velocity;  // Store velocity vector
+    float angle;
+    float dx, dy, dz;
 } Player;
 
 
@@ -68,50 +71,196 @@ typedef struct AABB {
     Vector3 max;
 } AABB;
 
-// Camera related functions
-void InitCamera(Camera3D *camera, const CameraSettings *cameraSettings);
-void CameraTests(Camera *camera);
-void GetCameraProjectionInfo(Camera *camera);
 
-// Game Update and Draw functions
-void UpdateGame(void);
-void UpdatePlayerCAM(Player *player, Camera3D *playerCamera);
-void DrawGame(void);
-void CheckCollisions(void);
-float playersDist(const Player *player, const Player *opponent);
+// bool CheckCollisionPLAYER(Player* PlayerBox, AABB* CollisionBox)
+// {
+//     // return CheckCollisionBoxes()
+
+// }
+
+// Initialization Functions
+void InitCamera(Camera3D *camera, const CameraSettings *cameraSettings)
+{
+    camera->fovy = cameraSettings->camFOVY;
+    camera->position = cameraSettings->camPosition;
+    camera->projection = cameraSettings->camProjection;
+    camera->target = cameraSettings->camTarget;
+    camera->up = cameraSettings->camUp;
+}
+void InitPlayerWithSettings(Player* player, PlayerSettings* ps)
+{
+    player->playerCamera = ps->Camera;
+    player->playerName = ps->Name;
+    player->currentPOS = ps->POS;
+    player->playerScore = ps->Score;
+    player->velocity = ps->Velocity;
+}
+
+void InitPlayerByValue(
+    Player* player, 
+    Camera3D camera, 
+    const char* name, 
+    Vector3 pos, 
+    double score, 
+    Vector3 v)
+{
+    player->playerCamera = &camera;
+    player->playerName = name;
+    player->currentPOS = &pos;
+    player->playerScore = &score;
+    player->velocity = &v;
+}
+
+void MovePlayer(Player *player, const float units)
+{
+    // if player turns past 2pi, subtract it 0 for next count
+    if (player->angle <= 0) {
+        player->angle += 2*PI;
+        
+    }
+    if (player->angle >= 2*PI) 
+    {
+        player->angle -= 2*PI;
+    }
+    if (IsKeyDown(KEY_W))
+    {
+        player->currentPOS->z += units;
+    }
+    if (IsKeyDown(KEY_S))
+    {
+        player->currentPOS->z -= units;
+    }
+    if (IsKeyDown(KEY_A))
+    {
+        player->currentPOS->x += units;
+    }
+    if (IsKeyDown(KEY_D))
+    {
+        player->currentPOS->x -= units;
+    }
+}
+void MoveCamera(Camera3D *cam, const float units)
+{
+    if (IsKeyDown(KEY_W))
+    {
+        cam->position.z += units;
+    }
+    if (IsKeyDown(KEY_S))
+    {
+        cam->position.z -= units;
+    }
+    if (IsKeyDown(KEY_A))
+    {
+        cam->position.x -= units;
+    }
+    if (IsKeyDown(KEY_D))
+    {
+        cam->position.x += units;
+    }
+}
+
+// Points u to v
+void SetVector(Vector3* u, Vector3* v)
+{
+    u = v;
+}
+void SetVectorPoints(Vector3* u, Vector3* v)
+{
+    u->x = v->x;
+    u->y = v->y;
+    u->z = v->z;
+}
+void CopyVector(Vector3* u, Vector3 pos)
+{
+    u->x = pos.x;
+    u->y = pos.y;
+    u->z = pos.z;
+}
+
+void SetPosByValue(Player* player, Vector3 pos)
+{
+    player->currentPOS->x = pos.x;
+    player->currentPOS->y = pos.y;
+    player->currentPOS->z = pos.z;
+}
+void SetPlayerPoint(Player* player, float x, float y, float z)
+{
+    player->currentPOS->x = x;
+    player->currentPOS->y = y;
+    player->currentPOS->z = z;
+}
+void SetPlayerVector(Player* player, Vector3* u)
+{
+    player->currentPOS = u;
+}
+
+void GetPlayerAngle(Player* player)
+{
+    double pdx, pdy;
+    
+}
+
+//**
+// // Game Update and Draw functions
+// void UpdateGame(void);
+// void UpdatePlayerCAM(Player *player, Camera3D *playerCamera);
+// void DrawGame(void);
+// void CheckCollisions(void);
+// float playersDist(const Player *player, const Player *opponent);
 
 
-// Level Functions
+// // Level Functions
 
-// Drawing Functions
-void DrawPlayerScore(const Player *player, const Camera3D *camera);
-
-
-// Player Movement and Interaction
-bool currentKey(KeyboardKey k); 
-void MoveCamera(Camera3D *camera);
-void MovePlayer(Player *player, const float units);
-void PlayerJump(Player *player, const float jumpHeight);
-void GetPlayerInfo(const Player *player);
-void DrawCameraInfo(const Camera3D *camera, const Player *player);
-void CameraInfo(Camera3D *camera);
-void UpdatePlayerCamera(Player *player);
+// // Drawing Functions
+// void DrawPlayerScore(const Player *player, const Camera3D *camera);
 
 
-// Score and Scoring
-void incrementPoints(Player *player, double points);
-void animatedScored(Player* player, Camera* cam);
-
-//Set Functions
-// bool setPOS(Player* player, Vector3* pos);
-// bool setPOS(Player* player, Vector3* pos));
-bool setPOS(Player* player, float x, float y, float z);
-bool setVelocity(Player* player, Vector3* v);
-bool setCamera(Player* player, Camera3D* Camera3D);
-bool setName(Player* player, const char* name);
-bool setScore(Player* player, double score);
+// // Player Movement and Interaction
+// bool currentKey(KeyboardKey k); 
+// void MoveCamera(Camera3D *cam, const float units);
+// void MovePlayer(Player *player, const float units);
+// void PlayerJump(Player *player, const float jumpHeight);
+// void GetPlayerInfo(const Player *player);
+// void DrawCameraInfo(const Camera3D *camera, const Player *player);
+// void CameraInfo(Camera3D *camera);
+// void UpdatePlayerCamera(Player *player);
 
 
-void debugDisplay(Player player, Camera* camera);
+// // Score and Scoring
+// void incrementPoints(Player *player, double points);
+// void animatedScored(Player* player, Camera* cam);
 
-void UpdatePlayerVelocity(Player *player);
+// //Set Functions
+// // bool setPOS(Player* player, Vector3* pos);
+// // bool setPOS(Player* player, Vector3* pos));
+// bool setPOS(Player* player, float x, float y, float z);
+// bool setVelocity(Player* player, Vector3* v);
+// bool setCamera(Player* player, Camera3D* Camera3D);
+// bool setName(Player* player, const char* name);
+// bool setScore(Player* player, double score);
+
+
+// void debugDisplay(Player player, Camera* camera);
+
+// void UpdatePlayerVelocity(Player *player);
+
+
+// void displayViewPortStats(Camera3D viewport_cam)
+// {
+//     DrawText(
+//             TextFormat(
+//                 "CAMERA\n
+//                 \tviewport_cam.fovy :\t % .2f\n
+//                 \tviewport_cam.position :\t % .2f\n
+//                 \tviewport_cam.projection :\t % .2f\n
+//                 \tviewport_cam.target :\t % .2f % .2f % .2f\n
+//                 \tviewport_cam.up :\t % .2f\n",
+//                 viewport_cam.fovy,
+//                 viewport_cam.position,
+//                 viewport_cam.projection,
+//                 viewport_cam.target.x,
+//                 viewport_cam.target.y,
+//                 viewport_cam.target.z,
+//                 viewport_cam.up),
+//             10, 280, 25, BLACK);
+// }
--- src/main.c
+++ src/main.c
@@ -10,33 +10,102 @@ int main()
     Vector3 cube_pos = {0.0f, 1.0f, 0.0f};
     Vector3 cubeSize = { 2.0f, 2.0f, 2.0f };
 
-    Camera3D main_cam = {0};
+    Camera3D main_cam = (Camera3D){0};
     main_cam.fovy = 45.0f;
-    main_cam.position = (Vector3){10, 10, 10};
-    main_cam.projection = CAMERA_ORTHOGRAPHIC;
+    main_cam.position = (Vector3){0, 0, -10};
+    main_cam.projection = CAMERA_PERSPECTIVE;
     main_cam.target = cube_pos;
     main_cam.up = (Vector3) {0, 1, 0};
-    //InitCamera(camera->main_cam, &settings);
 
-    
+    //InitCamera(camera->main_cam, &settings);
+    Camera3D player_cam = {0};
+    player_cam.fovy = 45.0f;
+    player_cam.position = (Vector3){10, 10, 10};
+    player_cam.projection = CAMERA_ORTHOGRAPHIC;
+    player_cam.target = cube_pos;
+    player_cam.up = (Vector3) {0, 1, 0};
+
+    Camera3D viewport_cam = (Camera3D){0};
+    viewport_cam.fovy = 45.0f;
+    viewport_cam.position = (Vector3){0, 0, 0};
+    viewport_cam.projection = CAMERA_PERSPECTIVE;
+    viewport_cam.target = cube_pos;
+    viewport_cam.up = (Vector3) {0, 1, 0};
+
 
     Player player;
-    Vector3 pos = {0};
-    player.currentPOS = &pos;
+    player.playerName = "p1";
+    player.currentPOS = &(Vector3){0};
+    player.playerScore = 0;
+    player.velocity = &(Vector3){0};
+    player.playerCamera = &(Camera3D){0};
 
-    Vector3 floor = {.x = 0, .y = 0, .z = 0};
+    Vector3 floor = {.x = 0, .y = -2, .z = 0};
+
+    // struct GUI_SETTINGS
+    // {
+    //     int screen_x;
+    //     int screen_y;
+    // };
+    //MoveCamera(&main_cam, 5.0f);
 
     while (!WindowShouldClose())
     {
-        if (IsCursorHidden()) UpdateCamera(&main_cam, CAMERA_FIRST_PERSON);
+        //if (IsCursorHidden()) UpdateCamera(&main_cam, CAMERA_FIRST_PERSON);
+        
         BeginDrawing();
-
         ClearBackground(WHITE);
-        DrawCube(cube_pos, 2, 2, 2, RED);
-        // DrawCube(floor, 100, 2, 100, RED);
+        DrawText(
+                    TextFormat(
+                        "CAMERA POS \n\tx:\t%.2f\n\ty:\t%.2f\n\tz:\t%.2f\n", 
+                        viewport_cam.position.x, 
+                        viewport_cam.position.y, 
+                        viewport_cam.position.z
+                    ), 
+                    10, 
+                    20, 
+                    25, 
+                    RED
+                );
+        DrawText(
+                TextFormat(
+                        "Player POS \n\tx:\t%.2f\n\ty:\t%.2f\n\tz:\t%.2f\n", 
+                        player.currentPOS->x, 
+                        player.currentPOS->y, 
+                        player.currentPOS->z
+                    ), 
+                    10, 
+                    130, 
+                    25, 
+                    RED
+                );
+        DrawText(
+            TextFormat(
+                "CAMERA \n\tviewport_cam.fovy :\t % .2f\n\tviewport_cam.position :\t % .2f\n\tviewport_cam.projection :\t % .2f\n\tviewport_cam.target :\t % .2f % .2f % .2f\n\tviewport_cam.up :\t % .2f\n ",
+                viewport_cam.fovy,
+                viewport_cam.position,
+                viewport_cam.projection,
+                viewport_cam.target.x,
+                viewport_cam.target.y,
+                viewport_cam.target.z,
+                viewport_cam.up),
+            10, 280, 25, BLACK);
 
+        BeginMode3D(viewport_cam);
+        viewport_cam.target = *player.currentPOS;
+        viewport_cam.position.y = player.currentPOS->y;
+        viewport_cam.position.z = player.currentPOS->z - 15;
+        viewport_cam.position.x = player.currentPOS->x;
+
+        DrawCube(floor, 100, 2, 100, RED);
+        //USE DELTA TIME
+        MovePlayer(&player, (5.0)/60);
+        DrawCube(cube_pos, 2, 2, 2, BLUE);
+        //PLAYER
+        DrawCube(*player.currentPOS, 2, 2, 2, YELLOW);
+
+        EndMode3D();
         EndDrawing();
     }
-    
     return 0;
 }
